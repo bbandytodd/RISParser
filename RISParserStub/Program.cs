709255ca13a1e23fb5f6e9c1f1871c24045d4467
Andy Todd <a.todd@building-blocks.com>
2012-05-09 14:25:07 +0100

    Added solution and sample file

--- RISParserStub/Program.cs
+++ RISParserStub/Program.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Reflection;
+using System.IO;
 using RISParser;
 
 namespace RISParserStub
 {
+    /// <summary>
+    /// This is a stub command line exe for demonstrating the library
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-
             RISParser.RISParser parser = new RISParser.RISParser();
-            parser.ParseRISDocument(@"C:\temp\ris.ris");
+            parser.ParseRISDocument(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\..\..\samples\ris.ris");
 
         }
     }
