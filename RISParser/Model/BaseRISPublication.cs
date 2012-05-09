2869a680868f4f137b031e675fc04d240917a7ea
Andy Todd <a.todd@building-blocks.com>
2012-05-09 14:15:59 +0100

    Adding as BB account

---
+++ RISParser/Model/BaseRISPublication.cs
@@ -0,0 +1,40 @@
+﻿namespace RISParser.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class BaseRISPublication : IRISPublication
+    {
+        private RISType _risType;
+        private string _title;
+        private List<Person> _authors;
+
+        public BaseRISPublication()
+        {
+            _authors = new List<Person>();
+        }
+
+        public RISType PublicationType
+        {
+            get { return this._risType; }
+            set { this._risType = value; }
+        }
+
+        public string Title
+        {
+            get { return this._title; }
+            set { this._title = value; }
+        }
+
+        public List<Person> Authors
+        {
+            get { return this._authors; }
+            set { this._authors = value; }
+        }
+    }
+}
