9e82f225caac2d427513b6426446edb2ff4199c7
Andy Todd <andy.blacktapes@gmail.com>
2012-05-09 13:59:30 +0100

    Initial add

---
+++ RISParser/Model/Person.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Pers.cs" company="Microsoft">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace RISParser.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+
+    public class Person
+    {
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ RISParserStub/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RISParser;
+
+namespace RISParserStub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            RISParser.RISParser parser = new RISParser.RISParser();
+            parser.ParseRISDocument(@"C:\temp\ris.ris");
+
+        }
+    }
+}
