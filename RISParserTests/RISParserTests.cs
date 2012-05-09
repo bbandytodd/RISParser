377a04636f3d6ac44b8b373514f85ad355849f0d
Andy Todd <a.todd@building-blocks.com>
2012-05-09 16:17:00 +0100

    Added unit test project (currently empty)

---
+++ RISParserTests/RISParserTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace RISParserTests
+{
+    /// <summary>
+    /// This class will hold tests for RISParser
+    /// </summary>
+    [TestFixture]
+    public class RISParserTests
+    {
+    }
+}
